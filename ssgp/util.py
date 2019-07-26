dfdcd7dfaaac33eaf8a0c6e6e22fdab1ea14cf79
Rafael Oliveira <oliveira.r.s.88@gmail.com>
2019-07-26 12:21:20 +1000

    Added tensor shape checking methods.

--- ssgp/util.py
+++ ssgp/util.py
@@ -1,10 +1,21 @@
 import torch
 
 def ensure_torch(value,dtype=torch.float32):
-    if not isinstance(value,torch.Tensor):
+    if not torch.is_tensor(value):
         value = torch.as_tensor(value,dtype=dtype)
     return value
 
 def ensure_positive(v):
     return torch.nn.functional.softplus(v)
 
+def check_exact_dim(x,dim,msg=None):
+    if not torch.is_tensor(x) or x.dim() != dim:
+        if msg is None:
+            msg = "Expected tensor of dimensionality {}".format(dim)
+        raise ValueError(msg)
+
+def check_size(x,size,dim=0,msg=None):
+    if not x.size(dim) == size:
+        if msg is None:
+            msg = "Expected tensor size at {} to be {}".format(dim,size)
+        raise ValueError(msg)
